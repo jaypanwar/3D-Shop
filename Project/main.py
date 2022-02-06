241aee6fd154ba0fa5749a5a91cb0bc4d68615e6
jaypanwar <61771490+jaypanwar@users.noreply.github.com>
2022-02-06 12:21:35 +0530

    Initial commit

---
+++ Project/main.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, Response,redirect,request,session,flash
+from camera import VideoCamera
+import os
+import psycopg2 #pip install psycopg2 
+import psycopg2.extras
+import re 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# from flaskext.mysql
+
+app = Flask(__name__)
+app.secret_key = 'Faizan-2122'
+
+DB_HOST = "localhost"
+DB_NAME = "user_db"
+DB_USER = "postgres"
+DB_PASS = "2122"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+@app.route('/Features',methods=['POST', 'GET'])
+def Features():
+    return render_template('Features.html')
+
+@app.route('/')
+def home():
+ # Check if user is loggedin
+    if 'loggedin' in session:
+    
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect('http://127.0.0.1:5000/login')
+    # return render_template('index.html')
+
+
+@app.route('/signup', methods= ['POST','GET'])
+def signup():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+ 
+    if request.method == 'POST' and 'username' in request.form and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+            
+        _hashed_password = generate_password_hash(password)
+        
+        #Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        print(account)
+        # If account exists show error and validation checks
+        if account:
+            flash('Account already exists!')    
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Invalid email address!')
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('Username must contain only characters and numbers!')
+        elif not username or not password or not email:
+            flash('Please fill out the form!')
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into users table
+            cursor.execute("INSERT INTO users (username,email, password) VALUES (%s,%s,%s)", (username,email,_hashed_password))
+            conn.commit()
+            flash('You have successfully registered!')
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        flash('Please fill out the form!')
+    # Show registration form with message (if any)    
+    return render_template('signup.html')
+
+@app.route('/login', methods= ['GET','POST'])
+def login():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        print(password)
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+ 
+        if account:
+            password_rs = account['password']
+            print(password_rs)
+            # If account exists in users table in out database
+            if check_password_hash(password_rs, password):
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                # Redirect to home page
+                return redirect('http://127.0.0.1:5000/')
+            else:
+                # Account doesnt exist or username/password incorrect
+                flash('Incorrect username/password')
+        else:
+            # Account doesnt exist or username/password incorrect
+            print('Incorrect username/password')
+
+    return render_template('login.html')
+    
+@app.route('/checkOut', methods=['POST', 'GET'])
+def checkOut():
+    return render_template('checkout.html')
+
+@app.route('/tryon/<file_path>',methods = ['POST', 'GET'])   
+def tryon(file_path):
+    file_path = file_path.replace(',','/')
+    os.system('python tryOn.py ' + file_path)
+    return redirect('http://127.0.0.1:5000/',code=302, Response=None)
+    #return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+CART=[]
+@app.route('/tryall',methods = ['POST', 'GET'])
+def tryall():
+    print("YESSS")
+    if request.method == 'POST':
+        cart = request.form['mydata'].replace(',', '/')
+        print("tryall cart type= ", type(cart))
+        print("tryall cart= ", cart)
+        os.system('python test.py ' + cart)
+        render_template('checkout.html', message='')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        #print("frame= ", frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route("/cart/<file_path>",methods = ['POST', 'GET'])
+def cart(file_path):
+    global CART
+    file_path = file_path.replace(',','/')
+    print("ADDED", file_path)
+    CART.append(file_path)
+    return render_template("checkout.html")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #    app.run(host="Localhost", port=5000)
+    # just for remambring
